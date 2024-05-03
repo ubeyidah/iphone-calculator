7b1186432ca2111b40b9e5ba24060a35ce4dc6eb
ubeyid oumer <ubeyidah@gmail.com>
2024-05-03 10:27:08 +0300

    display number in the page when click

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// DOM element
 const clockEl = document.querySelector('.clock');
 
 const acEl = document.querySelector('.ac');
 const pmEl = document.querySelector('.pm');
 const percentEl = document.querySelector('.percent');
+const displayEl = document.querySelector('.display');
 
 const divisionEl = document.querySelector(".division");
 const multiplicationEl = document.querySelector(".multiplication");
@@ -13,35 +13,43 @@ const equalEl = document.querySelector(".calculate");
 
 
 const decimalEl = document.querySelector('.dot');
-const zeroEl = document.querySelector('.zero');
-const num1El = document.querySelector('.num-1');
-const num2El = document.querySelector('.num-2');
-const num3El = document.querySelector('.num-3');
-const num4El = document.querySelector('.num-4');
-const num5El = document.querySelector('.num-5');
-const num6El = document.querySelector('.num-6');
-const num7El = document.querySelector('.num-7');
-const num8El = document.querySelector('.num-8');
-const num9El = document.querySelector('.num-9');
-
-
+const numbersEl = document.querySelectorAll('.number');
 
 
 
 // set up time
 
 const setUpTime = () => {
-    const today = new Date();
-    let hour = today.getHours();
-    let minute = today.getMinutes();
+  const today = new Date();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
 
-    // format hour
-    hour = hour > 12 ?  hour - 12 : hour;
+  // format hour
+  hour = hour > 12 ?  hour - 12 : hour;
 
-    // display on the page
-    clockEl.textContent = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`
+  // display on the page
+  clockEl.textContent = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`
 
 }
 
 setUpTime()
 setInterval(setUpTime,1000)
+
+
+// working on calculator
+const handleNumClick = (numStr) => {
+  if(displayEl.textContent === '0'){
+    displayEl.textContent = numStr;
+  }else{
+    displayEl.textContent += numStr;
+  }
+}
+
+
+// add eventlistner to all numbers 
+numbersEl.forEach(number => {
+  number.addEventListener('click', (e) => {
+   handleNumClick(e.target.textContent.toString())
+  })
+})
+
