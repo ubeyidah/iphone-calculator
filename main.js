cf5a7449274f1d5384d2988288f5d9d730d78342
ubeyid oumer <ubeyidah@gmail.com>
2024-05-03 14:48:39 +0300

    create main functionality

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ const pmEl = document.querySelector('.pm');
 const percentEl = document.querySelector('.percent');
 const displayEl = document.querySelector('.display');
 
-const divisionEl = document.querySelector(".division");
-const multiplicationEl = document.querySelector(".multiplication");
-const subtractionEl = document.querySelector(".subtraction");
-const additionEl = document.querySelector(".addition");
+const operatorsEl = document.querySelectorAll(".main-operator");
 const equalEl = document.querySelector(".calculate");
 
 
 const decimalEl = document.querySelector('.dot');
 const numbersEl = document.querySelectorAll('.number');
+
+let valueInMemory = null;
+let opratorInMemory = null;
 
 
 
@@ -37,19 +37,136 @@ setInterval(setUpTime,1000)
 
 
 // working on calculator
+
+const getValueAsStr = () =>  displayEl.textContent.split(",").join('');
+const getValueAsNum = () => parseFloat(getValueAsStr());
+const setValueAsStr = (valueStr) => {
+  if(valueStr[valueStr.length - 1 ] === '.'){
+    displayEl.textContent += "."
+    return;
+  }
+  const [wholeNum, decimalNum] = valueStr.split('.');
+  if(decimalNum){
+    displayEl.textContent = parseFloat(wholeNum).toLocaleString() + '.' + decimalNum;
+  }else {
+    displayEl.textContent = parseFloat(wholeNum).toLocaleString();
+  }
+}
+ 
+
 const handleNumClick = (numStr) => {
-  if(displayEl.textContent === '0'){
-    displayEl.textContent = numStr;
-  }else{
-    displayEl.textContent += numStr;
+  if(checkMaxLen()){
+    const currentDisplayStr = getValueAsStr()
+    checkMaxLen();
+    if(currentDisplayStr === '0'){
+      setValueAsStr(numStr);
+    }else{
+      setValueAsStr(currentDisplayStr + numStr)
+    }
   }
 }
 
+const getResultOfOpration = () => {
+  const valueNumMemory = parseFloat(valueInMemory);
+  const valueAsNum = getValueAsNum();
+  let newValue = null;
+    if(opratorInMemory === 'division'){
+      newValue = valueNumMemory / valueAsNum;
+    }else if(opratorInMemory === 'multiplication'){
+      newValue = valueNumMemory * valueAsNum;
+    }else if(opratorInMemory === 'subtraction'){
+      newValue = valueNumMemory - valueAsNum;
+    }else if(opratorInMemory ===  'addition'){
+      newValue = valueNumMemory + valueAsNum;
+    }
+  return newValue.toString();
+}
 
-// add eventlistner to all numbers 
+
+
+const handleOpratorClick = (opration) => {
+    checkMaxLen();
+    const currentValueAsStr = getValueAsStr();
+    if(!valueInMemory){
+      valueInMemory = currentValueAsStr;
+      opratorInMemory = opration;
+      setValueAsStr("0")
+      return;
+    }
+    
+    valueInMemory = getResultOfOpration();
+    setValueAsStr("0");
+    opratorInMemory = opration;
+  }
+
+// add eventlistner to all numbers  and decimal
 numbersEl.forEach(number => {
   number.addEventListener('click', (e) => {
    handleNumClick(e.target.textContent.toString())
   })
 })
 
+decimalEl.addEventListener('click' , () => {
+  const currentValueStr = getValueAsStr()
+  if(!currentValueStr.includes('.')){
+    setValueAsStr(currentValueStr + ".");
+  }
+})
+
+acEl.addEventListener('click', () => {
+  setValueAsStr("0");
+  opratorInMemory = null;
+  valueInMemory = null;
+  checkMaxLen();
+})
+
+pmEl.addEventListener('click', () => {
+  const currentValueNum = getValueAsNum();
+  const currentValueStr = getValueAsStr();
+  checkMaxLen();
+  if(currentValueNum > 0){
+    setValueAsStr('-' + currentValueStr);
+  }else if(currentValueNum < 0){
+    setValueAsStr((Math.abs(currentValueNum)).toString())
+  }
+})
+
+percentEl.addEventListener('click', ()=> {
+  checkMaxLen();
+  const currentValueNum = getValueAsNum()
+  const newValueNum = currentValueNum / 100;
+  setValueAsStr(newValueNum.toString())
+  opratorInMemory = null;
+  valueInMemory = null;
+})
+
+
+operatorsEl.forEach(operator => {
+  operator.addEventListener('click',(e) => {
+    const {value}  = e.target;
+    handleOpratorClick(value);
+  })
+})
+
+equalEl.addEventListener('click' ,() => {
+  if(valueInMemory){
+   setValueAsStr(getResultOfOpration());
+   checkMaxLen();
+  }
+})
+
+// check max length of numbers display
+function checkMaxLen(){
+ const value = displayEl.textContent;
+ if(value.length > 6){
+  displayEl.style.fontSize = '2.8rem'
+}else  if(value.length < 6){
+  displayEl.style.fontSize = '4rem'
+ }
+
+ if(value.length > 10){
+  return false;
+}else if(value.length < 11){
+  return true;
+ }
+}
