041e17d613c69faa315822bcc50f0226ab907ba6
ubeyid oumer <ubeyidah@gmail.com>
2024-05-03 10:00:29 +0300

    using built in js method to add zero before the time if it is less than 0

--- main.js
+++ main.js
@@ -13,11 +13,9 @@ const setUpTime = () => {
 
     // format hour
     hour = hour > 12 ?  hour - 12 : hour;
-    hour = hour < 10 ? `0${hour}` : hour;
-    minute = minute < 10 ? `0${minute}`  : minute;
 
     // display on the page
-    clockEl.textContent = `${hour}:${minute}`
+    clockEl.textContent = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`
 
 }
 
